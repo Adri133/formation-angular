4006dea2f638e25da5509cc04112042cc8217dfa
Adri1 <pignon.adrien@gmail.com>
2025-04-07 11:00:35 +0200

    Routing, navbar, home, footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
-import { Heart, HeartOff, LucideAngularModule } from 'lucide-angular';
+import { Heart, LucideAngularModule } from 'lucide-angular';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, ProductListComponent],
+  declarations: [AppComponent, ProductComponent, ProductListComponent, HomeComponent, NavbarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LucideAngularModule.pick({ Heart, HeartOff }),
+    LucideAngularModule.pick({ Heart }),
   ],
   providers: [],
   bootstrap: [AppComponent],
