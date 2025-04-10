ae93e1a79807cbad6bc3a357e80791f9852fb43b
Adri1 <pignon.adrien@gmail.com>
2025-04-10 20:10:13 +0200

    fix

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { filter, interval, map, Observable } from 'rxjs';
 
 @Component({
@@ -7,9 +7,13 @@ import { filter, interval, map, Observable } from 'rxjs';
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css',
 })
-export class CounterComponent {
-  counter$: Observable<string> = interval(1000).pipe(
-    filter((n) => n % 3 === 0),
-    map((n) => `${n} est ${n % 2 === 0 ? 'pair' : 'impair'}`)
-  );
+export class CounterComponent implements OnInit {
+  counter$!: Observable<string>;
+
+  ngOnInit() {
+    this.counter$ = interval(1000).pipe(
+      filter((n) => n % 3 === 0),
+      map((n) => `${n} est ${n % 2 === 0 ? 'pair' : 'impair'}`)
+    );
+  }
 }
